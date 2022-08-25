5a779f993098d2a6bc547a88372ef9840657c19a
kitkat1303 <85510980+kitkat1303@users.noreply.github.com>
2022-08-25 11:20:04 -0700

    linked database posts to front end

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,20 +1,30 @@
+import { Link } from "react-router-dom"
 import "./post.css"
 
-export default function Post() {
+export default function Post({post}) {
   return (
     <div className = "post">
-       <img className = "postImage" src="https://images.pexels.com/photos/6794043/pexels-photo-6794043.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+      {post.photo && (
+         <img className = "postImage" 
+         src={post.photo}
+         alt="" />
+      )}
+       
        <div className="postInfo">
-          <div className="postCat">
-             <span className="postCat">Music</span>
-             <span className="postCat">Life</span>
+          <div className="postCats">{
+            post.categories.map((c)=>(
+               <span className="postCat">{c.name}</span>
+            ))
+          }
+          <Link to = {`/post/${post._id}`}>
+          <span className="postTitle">{post.title}</span>
+          </Link>
           </div>
-          <span className="postTitle">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</span>
           <hr />
-          <span className="postDate">1 hour ago</span>
+          <span className="postDate">{new Date(post.createdAt).toDateString()}</span>
        </div>
        <p className = "postDesc">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero pariatur, aspernatur repellat suscipit commodi atque dolores nemo esse provident quos molestias officia dolore cupiditate a illum inventore dolor laboriosam facilis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero pariatur, aspernatur repellat suscipit commodi atque dolores nemo esse provident quos molestias officia dolore cupiditate a illum inventore dolor laboriosam facilis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero pariatur, aspernatur repellat suscipit commodi atque dolores nemo esse provident quos molestias officia dolore cupiditate a illum inventore dolor laboriosam facilis.
+          {post.desc}
        </p>
        </div>
   )
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,14 +3,27 @@ import SideBar from "../../components/sidebar/SideBar"
 import Header from "../../components/header/Header"
 import Posts from "../../components/posts/Posts"
 import "./home.css"
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(()=>{
+    const fetchPosts = async() =>{
+      const res = await axios.get("/posts");
+      setPosts(res.data)
+    }
+    fetchPosts()
+  }, []
+  )
+
   return (
     <>
    <Header/>
     <div className = "home">
-       <Posts/>
+       <Posts posts = {posts} />
        <SideBar/>
        </div>
        </>
